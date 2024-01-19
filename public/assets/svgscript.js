53ccc7e5d24fc40929fde2838a4018bc740dff5c
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2024-01-19 15:57:37 -0800

    Begin jQuery attempt

--- public/assets/svgscript.js
+++ public/assets/svgscript.js
@@ -1,20 +1,18 @@
+import { $, jQuery } from '@jquery';
+// export for others scripts to use
+//window.$ = $;
+//window.jQuery = jQuery;
+
 // Load the existing file and draw it
 
 SVG.on(document, 'DOMContentLoaded', function() {
    var draw = SVG().addTo("#svg-area").size(300, 300);
 
-    var oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", reqListener);
-    //oReq.open("GET", "https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/responsive-logo.svg");
-    oReq.open("GET", "/assets/US_House_2022.svg");
-    oReq.send();
-    
-    var extSVG ='';
-    function reqListener () {
-      //console.log(this.responseText);
-      extSVG = this.responseText;
-      console.log(extSVG);
-      draw = SVG(extSVG).addTo('#svg-area');//.size(800, 200);
-    }
-    //draw.rect(100, 100).attr({ fill: '#f06' });
+   $(document).ready(function() {
+        var image = SVG('#svg-area');
+        $.get('/assets/US_House_2022.svg', function(contents) {
+        var $tmp = $('svg', contents);
+        image.svg($tmp.html());
+        }, 'xml');
+  });
 });
