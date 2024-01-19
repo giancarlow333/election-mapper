560ebbd46aade53182762899481f1a7d9298a0fd
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2024-01-19 15:28:30 -0800

    Get it to load an external SVG file

--- public/assets/svgscript.js
+++ public/assets/svgscript.js
@@ -1,4 +1,21 @@
+// Load the existing file and draw it
+
 SVG.on(document, 'DOMContentLoaded', function() {
-    var draw = SVG().addTo('body').size(300, 300);
-    var rect = draw.rect(100, 100).attr({ fill: '#f06' });
+    var draw = SVG().addTo("#svg-area").size(300, 300);
+
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", reqListener);
+    oReq.open("GET", "https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/responsive-logo.svg");
+    oReq.send();
+    
+    var extSVG ='';
+    function reqListener () {
+      //console.log(this.responseText);
+      extSVG = this.responseText;
+      draw = SVG(extSVG).addTo('body');//.size(800, 200);
+    }
+
+
+
+    draw.rect(100, 100).attr({ fill: '#f06' });
 });
