c51089b18cf0bef6c5adc2608d84b1504437f806
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2024-01-19 15:34:09 -0800

    Attempt to load map from Wikipedia

--- public/assets/svgscript.js
+++ public/assets/svgscript.js
@@ -1,21 +1,20 @@
 // Load the existing file and draw it
 
 SVG.on(document, 'DOMContentLoaded', function() {
-    var draw = SVG().addTo("#svg-area").size(300, 300);
+   var draw = SVG().addTo("#svg-area").size(300, 300);
 
     var oReq = new XMLHttpRequest();
     oReq.addEventListener("load", reqListener);
-    oReq.open("GET", "https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/responsive-logo.svg");
+    //oReq.open("GET", "https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/responsive-logo.svg");
+    oReq.open("GET", "/assets/US_House_2022.svg");
     oReq.send();
     
     var extSVG ='';
     function reqListener () {
       //console.log(this.responseText);
       extSVG = this.responseText;
-      draw = SVG(extSVG).addTo('body');//.size(800, 200);
+      console.log(extSVG);
+      draw = SVG(extSVG).addTo('#svg-area');//.size(800, 200);
     }
-
-
-
-    draw.rect(100, 100).attr({ fill: '#f06' });
+    //draw.rect(100, 100).attr({ fill: '#f06' });
 });
