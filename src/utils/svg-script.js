0b435a44e9ea1302943162be20db623354cd04d5
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2024-01-19 14:40:18 -0800

    Draw a rectangle by appending to <body>

--- src/utils/svg-script.js
+++ src/utils/svg-script.js
@@ -1,9 +1,15 @@
 // Requires
 import { SVG } from '@svgdotjs/svg.js';
 // Element selectors
-let svgElt = document.querySelector("#svg-output");
+let svgElt = document.querySelector("#svg-area");
+console.log(svgElt);
 // https://stackoverflow.com/questions/22894540/creating-circles-with-svg-and-javascript
 const svgns = "http://www.w3.org/2000/svg";
 
-var draw = SVG().addTo("#svg-area").size(300, 300);
-draw.rect(100, 100).attr({ fill: '#f06' })
+var draw = SVG().addTo("body").size(300, 300);
+draw.rect(100, 100).attr({ fill: '#f06' });
+
+SVG.on(document, 'DOMContentLoaded', function() {
+    draw = SVG().addTo("#svg-area");
+    draw.rect(100, 100).attr({ fill: 'blue' });
+});
