84b7ab85624fe9d69588afcf513e6f5d3d63f495
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2024-01-19 17:45:51 -0800

    Get it to display the stored map!

--- public/assets/svgscript.js
+++ public/assets/svgscript.js
@@ -1,10 +1,11 @@
 // Load the existing file and draw it
 
 SVG.on(document, 'DOMContentLoaded', function() {
-   var draw = SVG().addTo("#svg-area").size(300, 300);
+   //var draw = SVG().addTo("#svg-area").size(300, 300);
+   // https://stackoverflow.com/questions/15911246/loading-a-svg-file-with-svg-js
 
    $(document).ready(function() {
-        var image = SVG('#svg-area');
+        var image = SVG('#svg-area').size(996, 501);
         $.get('./assets/US_House_2022.svg', function(contents) {
             var $tmp = $('svg', contents);
             image.svg($tmp.html());
