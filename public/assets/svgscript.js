4f2097b64293f499fea13ebfd416faa22d2ae070
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2024-01-19 17:32:10 -0800

    Get it to successfully pull the CONTENTS of the svg

--- public/assets/svgscript.js
+++ public/assets/svgscript.js
@@ -1,17 +1,10 @@
-const { JSDOM } = require( "jsdom" );
-const { window } = new JSDOM( "" );
-const $ = require( "jquery" )( window );
-// export for others scripts to use
-//window.$ = $;
-//window.jQuery = jQuery;
-
 // Load the existing file and draw it
 
 SVG.on(document, 'DOMContentLoaded', function() {
    var draw = SVG().addTo("#svg-area").size(300, 300);
 
    $(document).ready(function() {
-        var image = SVG('svg-area');
+        var image = SVG('#svg-area');
         $.get('./assets/US_House_2022.svg', function(contents) {
             var $tmp = $('svg', contents);
             image.svg($tmp.html());
