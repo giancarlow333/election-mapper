3c843a65a60fbbdaccc4f376a548213f83ce4641
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2024-01-19 17:21:00 -0800

    Begin converting to express instead

--- public/assets/svgscript.js
+++ public/assets/svgscript.js
@@ -1,4 +1,4 @@
-const { JSDOM } = require( "jsdom" );
+import { JSDOM } from "jsdom";
 const { window } = new JSDOM( "" );
 const $ = require( "jquery" )( window );
 // export for others scripts to use
