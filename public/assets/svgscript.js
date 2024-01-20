efb2b63255d023e58ea176a6f8a842e1a45c8e27
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2024-01-20 15:25:28 -0800

    Add Browserify

--- public/assets/svgscript.js
+++ public/assets/svgscript.js
@@ -12,3 +12,20 @@ SVG.on(document, 'DOMContentLoaded', function() {
         }, 'xml');
   });
 });
+
+const csv = require('csv-parser')
+const fs = require('fs')
+const results = [];
+
+fs.createReadStream('test_file.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    console.log(results);
+    // [
+    //   { NAME: 'Daffy Duck', AGE: '24' },
+    //   { NAME: 'Bugs Bunny', AGE: '22' }
+    // ]
+});
+
+
